950dde68819ef516c11cb00af9c01bb0963a9cca
Karol Sitarz <ka.sitarz@gmail.com>
2019-01-07 21:15:32 +0100

    implemented tmdb api

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
+import tmdb from '../apis/tmdb';
 
-export default () => (
-  <div>
-    Home
-  </div>
-);
+export default class Home extends Component {
+  componentDidMount () {
+    tmdb('/discover/movie', {
+      sort_by: 'popularity.desc'
+    }).then(res => console.log(res));
+  }
+
+  render () {
+    return <div>Home</div>;
+  }
+}
