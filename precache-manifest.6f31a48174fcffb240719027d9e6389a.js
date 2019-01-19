eca27fd333fbc9525933a66ba420581cfab38811
Karol Sitarz <ka.sitarz@gmail.com>
2019-01-19 15:38:10 +0100

    Updates

---
+++ precache-manifest.6f31a48174fcffb240719027d9e6389a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b92d55ae45d16f34615a",
+    "url": "/coderscamp-week5/static/js/runtime~main.b92d55ae.js"
+  },
+  {
+    "revision": "6125b68c71d3e077b3f4",
+    "url": "/coderscamp-week5/static/js/main.6125b68c.chunk.js"
+  },
+  {
+    "revision": "281d70c1688e729ad19c",
+    "url": "/coderscamp-week5/static/js/1.281d70c1.chunk.js"
+  },
+  {
+    "revision": "281d70c1688e729ad19c",
+    "url": "/coderscamp-week5/static/css/1.c22a736e.chunk.css"
+  },
+  {
+    "revision": "6940c709d7c1e3f42c9ec9a87a0e5cb8",
+    "url": "/coderscamp-week5/index.html"
+  }
+];
